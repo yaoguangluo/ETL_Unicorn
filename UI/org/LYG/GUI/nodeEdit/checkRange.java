e29737ba377fe6ef56ad3f569feb551f07b6c943
yaoguangluo <fengyue1985>
2019-01-31 09:33:22 +0800

    箭头链接磁力吸引效果，提高操作适用性。

--- UI/org/LYG/GUI/nodeEdit/checkRange.java
+++ UI/org/LYG/GUI/nodeEdit/checkRange.java
@@ -4,12 +4,12 @@ public class checkRange{
 	public checkRange(linkNode first,linkNode node, MouseEvent arg0) {
 		first=new Sort().sort(first);
 		linkNode node1=new linkNode();
-	    node1=first;
-	    if(node1!=null){
-	    	int x,y;
+		node1=first;
+		if(node1!=null){
+			int x,y;
 			x=arg0.getX();
 			y=arg0.getY();
-			if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y)&&(y<node1.y+16)&&(node.ID!=node1.ID)){
+			if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y)&&(y<node1.y+16)&&(node.ID!=node1.ID)){
 				node1.beconnect=true;
 				node1.tbeconnect=true;
 				node1.tbeconnectx=node.x;
@@ -18,7 +18,7 @@ public class checkRange{
 				node1.tbeconnetName=new String(node.name);
 				return;
 			}
-			if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y+16)&&(y<node1.y+32)&&(node.ID!=node1.ID)){
+			if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y+16)&&(y<node1.y+32)&&(node.ID!=node1.ID)){
 				node1.beconnect=true;
 				node1.mbeconnect=true;
 				node1.mbeconnectx=node.x;
@@ -27,7 +27,7 @@ public class checkRange{
 				node1.mbeconnetName=new String(node.name);
 				return;
 			}	
-			if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y+32)&&(y<node1.y+50)&&(node.ID!=node1.ID)){
+			if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y+32)&&(y<node1.y+50)&&(node.ID!=node1.ID)){
 				node1.beconnect=true;
 				node1.dbeconnect=true;
 				node1.dbeconnectx=node.x;
@@ -40,7 +40,7 @@ public class checkRange{
 				node1=node1.next;
 				x=arg0.getX();
 				y=arg0.getY();
-				if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y)&&(y<node1.y+16)&&(node.ID!=node1.ID)){
+				if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y)&&(y<node1.y+16)&&(node.ID!=node1.ID)){
 					node1.beconnect=true;
 					node1.tbeconnect=true;
 					node1.tbeconnectx=node.x;
@@ -49,7 +49,7 @@ public class checkRange{
 					node1.tbeconnetName=new String(node.name);
 					return;
 				}
-				if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y+16)&&(y<node1.y+32)&&(node.ID!=node1.ID)){
+				if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y+16)&&(y<node1.y+32)&&(node.ID!=node1.ID)){
 					node1.beconnect=true;
 					node1.mbeconnect=true;
 					node1.mbeconnectx=node.x;
@@ -58,7 +58,7 @@ public class checkRange{
 					node1.mbeconnetName=new String(node.name);
 					return;
 				}	
-				if((x>node1.x)&&(x<node1.x+50)&&(y>node1.y+32)&&(y<node1.y+50)&&(node.ID!=node1.ID)){
+				if((x>node1.x-20)&&(x<node1.x+50)&&(y>node1.y+32)&&(y<node1.y+50)&&(node.ID!=node1.ID)){
 					node1.beconnect=true;
 					node1.dbeconnect=true;
 					node1.dbeconnectx=node.x;
@@ -68,6 +68,6 @@ public class checkRange{
 					return;
 				}
 			}
-	    }
+		}
 	}
 }
