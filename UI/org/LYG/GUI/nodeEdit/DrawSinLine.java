2392cee56fb0366e53ca7759db0c64112da13b76
yaoguangluo <fengyue1985>
2019-02-23 06:52:38 +0800

    flashing filter in memory

--- UI/org/LYG/GUI/nodeEdit/DrawSinLine.java
+++ UI/org/LYG/GUI/nodeEdit/DrawSinLine.java
@@ -5,10 +5,9 @@ import org.LYG.GUI.theme.neroCell.DrawNeroCellMask31;
 import org.LYG.GUI.theme.neroCell.DrawNeroCellMask32;
 public class DrawSinLine{
 	public static void drawCosLine(int x0, int y0, Graphics2D g2) {
-		DrawNeroCellMask31 drawMask = new DrawNeroCellMask31();
-		for(int y = 0; y < drawMask.neroShape.length; y++) {
-			for(int x = 0; x < drawMask.neroShape[0].length; x++) {
-				if(1 == drawMask.neroShape[y][x]) {
+		for(int y = 0; y < DrawNeroCellMask31.neroShape.length; y++) {
+			for(int x = 0; x < DrawNeroCellMask31.neroShape[0].length; x++) {
+				if(1 == DrawNeroCellMask31.neroShape[y][x]) {
 					g2.drawLine(x + x0, y + y0, x + x0, y + y0);
 				}
 			}
@@ -16,10 +15,9 @@ public class DrawSinLine{
 	}
 
 	public static void drawSinLine(int x0, int y0, Graphics2D g2) {
-		DrawNeroCellMask32 drawMask = new DrawNeroCellMask32();
-		for(int y = 0; y < drawMask.neroShape.length; y++) {
-			for(int x = 0; x < drawMask.neroShape[0].length; x++) {
-				if(1 == drawMask.neroShape[y][x]) {
+		for(int y = 0; y < DrawNeroCellMask32.neroShape.length; y++) {
+			for(int x = 0; x < DrawNeroCellMask32.neroShape[0].length; x++) {
+				if(1 == DrawNeroCellMask32.neroShape[y][x]) {
 					g2.drawLine(x + x0, y + y0, x + x0, y + y0);
 				}
 			}
