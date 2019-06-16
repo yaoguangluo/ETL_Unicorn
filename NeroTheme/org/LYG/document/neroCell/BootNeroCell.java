1bf1d165d5d10cb595a2961b8ab224e700214d64
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-06-16 16:03:31 +0800

    2个文件， 一个是一键档案运行， 一个是一键ETL界面执行。 20190616 罗瑶光。

---
+++ NeroTheme/org/LYG/document/neroCell/BootNeroCell.java
@@ -0,0 +1,8 @@
+package org.LYG.document.neroCell;
+
+import org.LYG.GUI.nodeEdit.LinkNode;
+public class BootNeroCell{
+	public static void bootCell(LinkNode linkNode) {
+		
+	}
+}
---
+++ NeroTheme/org/LYG/document/neroCell/BootNeroDoc.java
@@ -0,0 +1,9 @@
+package org.LYG.document.neroCell;
+
+import org.LYG.GUI.nodeEdit.LinkNode;
+
+public class BootNeroDoc{
+	public static void bootCell(LinkNode linkNode) {
+		
+	}
+}
