6c076fe49318fbc72f16b738aa1f67ec9008e299
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-06-18 10:26:22 +0800

    文档执行线程已经好了，准备vpcs布局

--- NeroTheme/org/LYG/document/neroCell/BootNeroDoc.java
+++ NeroTheme/org/LYG/document/neroCell/BootNeroDoc.java
@@ -1,9 +1,48 @@
 package org.LYG.document.neroCell;
-
+import java.io.File;
+import javax.swing.JTextPane;
+import org.LYG.GUI.nodeEdit.LinkList;
 import org.LYG.GUI.nodeEdit.LinkNode;
-
-public class BootNeroDoc{
-	public static void bootCell(LinkNode linkNode) {
-		
+import org.LYG.GUI.nodeView.NodeShow;
+import org.LYG.document.load.LoadFile;
+import org.LYG.sets.stable.StableData;
+public class BootNeroDoc extends Thread implements Runnable{
+	private String fileCurrentpath;
+	private LinkList first;
+	private NodeShow nodeView;
+	private JTextPane rightBotJTextPane;
+	public BootNeroDoc(LinkList first, String fileCurrentpath, NodeShow nodeView
+			, JTextPane rightBotJTextPane) {
+		this.fileCurrentpath= fileCurrentpath;
+		this.first= first;
+		this.rightBotJTextPane= rightBotJTextPane;
+	}
+	public void run() {
+		try {
+			System.out.println(fileCurrentpath);
+			if(null== fileCurrentpath|| fileCurrentpath.isEmpty()|| !fileCurrentpath.contains
+					(StableData.FILE_FORMAT_ETL)) {
+				System.out.println(StableData.ATTENSION_RECHOICE);
+				return;
+			}
+			File file= new File(fileCurrentpath);
+			if(!file.isFile()) {
+				System.out.println(StableData.ATTENSION_RECHOICE);
+				return;
+			}
+			LinkNode needDeleteNode= first.first;
+			while(needDeleteNode!= null) {
+				first.first= first.deletNode(first.first, needDeleteNode.name, needDeleteNode.ID
+						, needDeleteNode.primaryKey);
+				if(null== needDeleteNode.next) {
+					break;
+				}
+				needDeleteNode= needDeleteNode.next;
+			}	
+			first.first= LoadFile.Load(first.first, nodeView, file, first);
+			BootNeroCell.bootCell(first.first, rightBotJTextPane);
+		}catch(Exception loadE) {
+			loadE.printStackTrace();
+		}
 	}
 }
